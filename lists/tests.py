132cc61bd295438e5887d485b8155e26dd991293
gilgamesh7 <felidaesrule@yahoo.com>
2022-07-14 14:14:25 +0530

    First unit test in tets.py , set up first route

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-# Create your tests here.
+from django.urls import resolve
+from lists.views import home_page
+
+import pytest
+
+def test_root_url_resolves_to_home_page_view():
+    found = resolve('/home/home/')
+    print(found.func)
+    assert found.func == home_page
